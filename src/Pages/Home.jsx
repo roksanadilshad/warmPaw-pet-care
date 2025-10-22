cbf59e460dc8b16f248b2c3a25c67778b961a15a
Roksana Dilshad <roksanadilshad@gmail.com>
2025-10-22 11:13:20 +0600

    Add card UI

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,17 +12,18 @@ const Home = () => {
            <HeroSlider></HeroSlider>
     <section>
       <div>
-        <div>
-         <h2 className='text-4xl text-center font-bold w-full mx-auto py-20'>Popular Winter Care Services</h2>
+        <div className='w-full mx-auto py-20'>
+         <h2 className='text-4xl text-center font-bold pb-4'>Popular Winter Care Services</h2>
+         <p class="text-gray-700 text-center max-w-2xl mx-auto mb-8">
+  As the chilly season approaches, keeping yourself, your home, and your pets safe and comfortable is essential. 
+</p>
         </div>
-        <div className='grid grid-cols-3 w-11/12 mx-auto'>
+        <div className='grid grid-cols-3 gap-10 w-11/12 mx-auto'>
           {
             pets.map(petCard => <PetsCard key={petCard.serviceId
 } petCard={petCard}></PetsCard>)
           }
         </div>
-      
-
       </div>
       </section>       
         </div>
--- src/Pages/PetsCard.jsx
+++ src/Pages/PetsCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import usePets from '../Hooks/usePets';
+import { IoStar, IoStarOutline } from 'react-icons/io5';
 
 const PetsCard = ({petCard = []}) => {
     //const pets = useLoaderData();
@@ -11,14 +11,24 @@ const PetsCard = ({petCard = []}) => {
     return (
         <div>
 
-            <div className="card bg-base-100 shadow-lg rounded-2xl hover:scale-105 transition-all">
-  <figure><img src={image} className='w-full' alt="Dog Coat" /></figure>
+            <div className="bg-amber-50 card shadow-lg rounded-2xl hover:scale-105 transition-all">
+  <figure><img src={image} className='rounded-xl w-full h-96 object-cover' alt="Dog Coat" /></figure>
   <div className="card-body">
-    <h2 className="card-title">{serviceName}</h2>
-    <p>{price}$</p>
-    <p>{rating}</p>
+    <h2 className="text-xl font-semibold">{serviceName}</h2>
+    <table>
+        <tbody>
+        <tr>
+            <td className='font-semibold text-neutral'>Price</td>
+            <td>{price}$</td>
+        </tr>
+        <tr>
+            <td className='font-semibold text-neutral'>Ratings</td>
+            <td className='flex items-center gap-1'>{rating}<IoStar className='text-amber-400' /></td>
+        </tr>
+        </tbody>
+    </table>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary btn-sm">View Details</button>
+      <button className="btn btn-warning w-full">View Details</button>
     </div>
   </div>
 </div>

7f711ec47d40bb09277a97ef9e8085e911c8ac3b
Roksana Dilshad <roksanadilshad@gmail.com>
2025-10-22 22:56:59 +0600

    Add icon

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,9 +1,12 @@
 import React, { use, useState } from 'react';
 import { AuthContext } from '../Context/AuthContext';
 import { Link } from 'react-router';
+import { RiEdit2Fill } from 'react-icons/ri';
 
 const Profile = () => {
     const {user, updateUserProfile} = use(AuthContext)
+    const [isEditing, setIsEditing] = useState(false);
+
     //console.log(user);
 
   const handleUpdate = (e) => {
@@ -22,25 +25,32 @@ const Profile = () => {
     
     return (
 <div className='h-screen'>
-        <div className="max-w-md  mx-auto bg-gradient-to-r from-[#f1dcaa] to-[#FFB347] p-6 rounded-2xl shadow-lg mt-10">
+        <div className="max-w-3xl  mx-auto bg-gradient-to-r from-[#f1dcaa] to-[#FFB347] p-6 rounded-2xl shadow-lg mt-20">
              {user && (
         <div className="mt-6 text-center">
           <img
             src={user.photoURL}
             alt="Profile"
-            className="w-24 h-24 rounded-full mx-auto mb-2"
+            className="w-32 h-32 rounded-full mx-auto mb-2"
           />
           <h3 className="text-lg font-semibold">{user.displayName}</h3>
           <p>{user.email}</p>
         </div>
       )}
-      <h2 className="text-2xl font-bold text-center mb-4">Update Profile</h2>
-      <div className="flex flex-col gap-4">
-        <form onSubmit={handleUpdate}>
+      <div  className='border-[1px] rounded-xl my-4 border-[#b6761d8c] py-4'>
+        <div className='flex justify-center items-center'>
+      <button onClick={() => setIsEditing(true)} className="text-2xl font-bold text-center mb-4">Update Profile</button>
+       <RiEdit2Fill />
+
+        </div>
+        {
+            isEditing && (
+                    <form onSubmit={handleUpdate} className="flex flex-col gap-4 items-center ">
             <input
           type="text"
           placeholder="Enter new name"
           name='name'
+          defaultValue={user.displayName || "Un Known"}
           className="input input-bordered"
         />
         <input
@@ -48,12 +58,24 @@ const Profile = () => {
           placeholder="Enter new photo URL"
           name='photoURL'
           className="input input-bordered"
+           defaultValue={user.photoURL || "https://images.unsplash.com/photo-1747592771443-e15f155b1faf?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyN3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=500"}
         />
-        <button className="btn btn-success text-white">
-          Update Profile
-        </button>
-        {message && <p className="text-center mt-2">{message}</p>}
+        <div className="flex gap-2">
+      <button type="submit" className="btn btn-success flex-1">
+        Save
+      </button>
+      <button
+        type="button"
+        onClick={() => setIsEditing(false)}
+        className="btn btn-error flex-1"
+      >
+        Cancel
+      </button>
+    </div>
         </form>
+            )
+        }
+        
       </div>
 
      
